4fabe7213c31ceffe0e9bc794407c903eb01e744
filipovskii_off <filipovskii.off@gmail.com>
2012-03-02 02:56:31 +0400

    more html stuff

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp static/* _build/ -r
 b=_build/index.html
-pandoc cv.md -thtml5 -o $b -chttp://twitter.github.com/bootstrap/assets/css/bootstrap.css
+pandoc cv.md -thtml5 -o $b -chttp://twitter.github.com/bootstrap/assets/css/bootstrap.css --include-before-body=body-header.html --include-after-body=body-footer.html
 
 pandoc cv.md -o _build/cv.pdf
