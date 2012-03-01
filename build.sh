4faef64f79384fa4bab7da2eea190cd315d7202f
filipovskii_off <filipovskii.off@gmail.com>
2012-03-02 02:10:04 +0400

    html build changes

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 cp static/* _build/ -r
 b=_build/index.html
-echo "<html><head><link rel='stylesheet' href='css/bootstrap.css'/></head>" > $b
-echo "</head><body style='background-color:whiteSmoke'>" >> $b
-echo "<div class='container'>" >> $b
-markdown cv.md >> $b
-echo "</div></body></html>" >> $b
+pandoc cv.md -thtml5 -o $b -chttp://twitter.github.com/bootstrap/assets/css/bootstrap.css
 
 pandoc cv.md -o _build/cv.pdf
