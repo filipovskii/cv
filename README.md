72ec626cb9d9c915d99ad4cf81036cecd140718e
filipovskii_off <filipovskii.off@gmail.com>
2012-03-11 14:19:49 +0400

    it builds! buiiiilddsss!!!

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 pandoc 1.9 with latex support is required for build. To install them in ubuntu:
 
     sudo aptitude install texlive
+    sudo aptitude install texlive-latex-recommended
+    sudo aptitude install texlive-lang-cyrillic
     sudo aptitude install cabal
     cabal install pandoc
--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ cp static/* _build/ -r
 b=_build/index.html
 pandoc cv.md -thtml5 -o $b -chttp://twitter.github.com/bootstrap/assets/css/bootstrap.css --include-before-body=body-header.html --include-after-body=body-footer.html
 
-pandoc --template=template/resume-template.tex --latex-engine=xelatex cv.md -o _build/cv.pdf
+pandoc --template=template/resume-template.tex cv.md -o _build/cv.pdf
